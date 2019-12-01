0a6f94019334f4ce6beaa039dac759b442e3ac1b
Matheus Gabriel Werny de Lima <matheusgwdl@protonmail.com>
2019-12-01 22:12:52 +0100

    initial commit

---
+++ src/Endpoints.cpp
@@ -0,0 +1,36 @@
+#include "ig/Endpoints.h"
+#include "tools/http/HttpClient.h"
+#include "tools/http/InputFile.h"
+#include <iostream>
+#include "tools/Tools.h"
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+
+namespace ig
+{
+	Endpoints::Endpoints(const std::string &username, const std::string &password) : m_username(username), m_password(password)
+	{}
+
+	bool Endpoints::login() const
+	{
+		boost::uuids::uuid uuid = boost::uuids::random_generator()();
+
+		//req nach si/fetch_headers/?challenge_type=signup&guid=
+
+		//http args
+		std::vector<tools::HttpArg> http_args;
+		http_args.push_back(tools::HttpArg("chat_id", chat_id));
+		http_args.push_back(tools::HttpArg("text", text));
+		http_args.push_back(tools::HttpArg("parse_mode", parse_mode));
+		http_args.push_back(tools::HttpArg("disable_web_page_preview", disable_web_page_preview));
+		http_args.push_back(tools::HttpArg("disable_notification", disable_notification));
+		http_args.push_back(tools::HttpArg("reply_to_message_id", reply_to_message_id));
+		http_args.push_back(tools::HttpArg("reply_markup", reply_markup->parse_to_json()));
+
+		tools::HttpClient http_client("https://api.telegram.org/bot" + m_token + "/sendMessage", http_args);
+		std::string json = http_client.send_post_req_multipart().txt;
+
+
+	}
+}
