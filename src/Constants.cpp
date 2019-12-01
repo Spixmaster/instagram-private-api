1f2912347f9f413242abdf739cb2ac12ba6e065f
Spixmaster <spixmaster528@gmail.com>
2019-12-01 22:12:52 +0100

    initial commit

---
+++ src/Constants.cpp
@@ -0,0 +1,9 @@
+#include "ig/Constants.h"
+#include <string>
+#include <iostream>
+
+namespace ig
+{
+	const std::string Constants::version = "0.0.1";
+	const std::string Constants::release = "01.Dec.2019";
+}
