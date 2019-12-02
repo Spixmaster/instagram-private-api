32353859ccf894429addbdcfb559ee6dcf4414f3
Matheus Gabriel Werny de Lima <matheusgwdl@protonmail.com>
2019-12-02 21:50:55 +0100

    just saving

--- src/Endpoints.cpp
+++ src/Endpoints.cpp
@@ -6,6 +6,8 @@
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_generators.hpp>
 #include <boost/uuid/uuid_io.hpp>
+#include "tools/http/HttpHeader.h"
+#include "ig/Constants.h"
 
 namespace ig
 {
@@ -16,17 +18,20 @@ namespace ig
 	{
 		boost::uuids::uuid uuid = boost::uuids::random_generator()();
 
-		//req nach si/fetch_headers/?challenge_type=signup&guid=
+		//http headers
+		std::vector<tools::HttpHeader> http_headers;
+		http_headers.push_back(tools::HttpHeader("Connection", "close"));
+		http_headers.push_back(tools::HttpHeader("Accept", "*/*"));
+		http_headers.push_back(tools::HttpHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8"));
+		http_headers.push_back(tools::HttpHeader("Cookie2", "$Version=1"));
+		http_headers.push_back(tools::HttpHeader("Accept-Language", "en-US"));
+		http_headers.push_back(tools::HttpHeader("User-Agent", Constants::ig_user_agent));
 
 		//http args
 		std::vector<tools::HttpArg> http_args;
 		http_args.push_back(tools::HttpArg("chat_id", "asdf"));
 
-		//http args
-		std::vector<tools::HttpArg> http_args;
-		http_args.push_back(tools::HttpArg("chat_id", "asdf"));
-
-		tools::HttpClient http_client("https://api.telegram.org/bot" + m_token + "/sendMessage", http_args);
+		tools::HttpClient http_client(Constants::ig_url + "si/fetch_headers/?challenge_type=signup&guid=" + uuid, http_args);
 		std::string json = http_client.send_post_req_multipart().txt;
 
 
