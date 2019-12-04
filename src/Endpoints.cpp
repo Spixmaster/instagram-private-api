3915aae27002ada6b884859ddeba823fa8115335
Matheus Gabriel Werny de Lima <matheusgwdl@protonmail.com>
2019-12-04 08:06:26 +0100

    checkpoint

--- src/Endpoints.cpp
+++ src/Endpoints.cpp
@@ -8,6 +8,7 @@
 #include <boost/uuid/uuid_io.hpp>
 #include "tools/http/HttpHeader.h"
 #include "ig/Constants.h"
+#include <boost/uuid/detail/md5.hpp>
 
 namespace ig
 {
@@ -16,27 +17,54 @@ namespace ig
 
 	bool Endpoints::login() const
 	{
-		std::string uuid = boost::uuids::to_string(boost::uuids::random_generator()());
+		//#####first request#####
+		{
+			std::string uuid = boost::uuids::to_string(boost::uuids::random_generator()());
+			uuid.erase(std::remove(uuid.begin(), uuid.end(), '-'), uuid.end());
 
-		//http headers
-		std::vector<tools::HttpHeader> http_headers;
-		http_headers.push_back(tools::HttpHeader("Connection", "close"));
-		http_headers.push_back(tools::HttpHeader("Accept", "*/*"));
-		http_headers.push_back(tools::HttpHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8"));
-		http_headers.push_back(tools::HttpHeader("Cookie2", "$Version=1"));
-		http_headers.push_back(tools::HttpHeader("Accept-Language", "en-US"));
-		http_headers.push_back(tools::HttpHeader("User-Agent", Constants::ig_user_agent));
+			//http headers
+			std::vector<tools::HttpHeader> http_headers;
+			http_headers.push_back(tools::HttpHeader("Connection", "close"));
+			http_headers.push_back(tools::HttpHeader("Accept", "*/*"));
+			http_headers.push_back(tools::HttpHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8"));
+			http_headers.push_back(tools::HttpHeader("Cookie2", "$Version=1"));
+			http_headers.push_back(tools::HttpHeader("Accept-Language", "en-US"));
+			http_headers.push_back(tools::HttpHeader("User-Agent", Constants::ig_user_agent));
 
-		//http args
-		std::vector<tools::HttpArg> http_args;
-		//todo normally, url query but I do not like that
-		http_args.push_back(tools::HttpArg("challenge_type", "signup"));
-		http_args.push_back(tools::HttpArg("guid", uuid));
+			tools::HttpClient http_client(Constants::ig_url + "si/fetch_headers/?challenge_type=signup&guid=" + uuid, http_headers);
+			std::string json = http_client.send_post_req_urlencoded().txt;
+		}
 
+		//#####second request#####
+		{
+			std::string uuid = boost::uuids::to_string(boost::uuids::random_generator()());
 
-		tools::HttpClient http_client(Constants::ig_url + "si/fetch_headers/", http_headers, http_args);
-		std::string json = http_client.send_post_req_urlencoded().txt;
+			//http headers
+			std::vector<tools::HttpHeader> http_headers;
+			http_headers.push_back(tools::HttpHeader("Connection", "close"));
+			http_headers.push_back(tools::HttpHeader("Accept", "*/*"));
+			http_headers.push_back(tools::HttpHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8"));
+			http_headers.push_back(tools::HttpHeader("Cookie2", "$Version=1"));
+			http_headers.push_back(tools::HttpHeader("Accept-Language", "en-US"));
+			http_headers.push_back(tools::HttpHeader("User-Agent", Constants::ig_user_agent));
 
+			//http args
+			std::vector<tools::HttpArg> http_args;
+			http_args.push_back(tools::HttpArg("phone_id", "EERRRRRRRRRRRRRRRR"));
+			http_args.push_back(tools::HttpArg("_csrftoken", "EERRRRRRRRRRRRRRRR"));
+			http_args.push_back(tools::HttpArg("username", m_username));
+			http_args.push_back(tools::HttpArg("guid", uuid));
+			http_args.push_back(tools::HttpArg("device_id", "EERRRRRRRRRRRRRRRR"));
+			http_args.push_back(tools::HttpArg("password", m_password));
+			http_args.push_back(tools::HttpArg("login_attempt_count", 0));
 
+//			data = {'phone_id': self.generateUUID(True),
+//					'_csrftoken': self.LastResponse.cookies['csrftoken'],
+//					'username': self.username,
+//					'guid': self.uuid,
+//					'device_id': self.device_id,
+//					'password': self.password,
+//					'login_attempt_count': '0'}
+		}
 	}
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
+#include "ig/SpecialTools.h"
+#include <iostream>
+
 int main()
 {
+	std::string temp = ig::SpecialTools::md5_hash("abc1234");
+
+	std::cout << temp << std::endl;
+
 	return 0;
 }
