7b121044d8b5d625d85ee0c1efeb25ad0a241c39
Spixmaster <spixmaster528@gmail.com>
2019-12-14 22:09:14 +0100

    working on login

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -4,15 +4,17 @@
 
 namespace ig
 {
-	const std::string Constants::version = "0.0.11";
-	const std::string Constants::release = "06.Dec.2019";
+	const std::string Constants::version = "0.0.12";
+	const std::string Constants::release = "14.Dec.2019";
+
 	const std::string Constants::ig_url = "https://i.instagram.com/api/v1/";
+	const std::string Constants::ig_sig_key_version = "4";
+	const std::string Constants::ig_sig_key = "19ce5f445dbfd9d29c59dc2a78c616a7fc090a8e018b9267bc4240a30244c53b";
+
 	const std::string Constants::device_manufacturer = "Xiaomi";
 	const std::string Constants::device_model = "HM 1SW";
 	const std::string Constants::device_android_version = "18";
 	const std::string Constants::device_android_release = "4.3";
 	const std::string Constants::ig_user_agent = "Instagram 10.26.0 Android (" + Constants::device_android_version +
 			"/" + Constants::device_android_release + "; 320dpi; 720x1280; " + Constants::device_manufacturer + "; " + Constants::device_model + "; armani; qcom; en_US)";
-	const std::string Constants::ig_sig_key = "c36436a942ea1dbb40d7f2d7d45280a620d991ce8c62fb4ce600f0a048c32c11";
-	const std::string Constants::ig_sig_key_version = "4";
 }
