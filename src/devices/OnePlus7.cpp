6905427f04aafe7c173154d8e98e3e68fcfb46e3
Matheus Gabriel Werny de Lima <matheusgwdl@protonmail.com>
2019-12-19 23:07:47 +0100

    rewrite lost data

---
+++ src/devices/OnePlus7.cpp
@@ -0,0 +1,18 @@
+#include "include/ig/devices/OnePlus7.h"
+
+namespace ig
+{
+	const std::string OnePlus7::ig_version = "122.0.0.29.238";
+	const std::string OnePlus7::android_version = "28";
+	const std::string OnePlus7::android_release = "9.0";
+	const std::string OnePlus7::dpi = "420dpi";
+	const std::string OnePlus7::resolution = "1080x2260";
+	const std::string OnePlus7::manufacturer = "OnePlus";
+	const std::string OnePlus7::device = "GM1903";
+	const std::string OnePlus7::model = "OnePlus7";
+	const std::string OnePlus7::cpu = "qcom";
+	const std::string OnePlus7::user_agent = "Instagram " + OnePlus7::ig_version + " Android (" +
+			OnePlus7::android_version + "/" + OnePlus7::android_release + "; " + OnePlus7::dpi + "; " +
+			OnePlus7::resolution + "; " + OnePlus7::manufacturer + "; " + OnePlus7::device +
+			"; " + OnePlus7::model + "; " + OnePlus7::cpu + "; en_US)";
+}
