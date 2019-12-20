131911110a8cf769697e1ca78143c3cea7b3b7f1
Spixmaster <spixmaster528@gmail.com>
2019-12-20 08:48:52 +0100

    just saving

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
-#include "ig/Endpoints.h"
+#include "ig/Api.h"
 #include "tools/Tools.h"
+#include <variant>
+
+void hi(std::variant<std::string, int> hi)
+{
+	std::cout << "juten tag" << std::endl;
+}
 
 int main()
 {
-	ig::Endpoints api("hija_look", "generic_password");
+	ig::Api api("hija_look", "generic_password");
+
+	hi(5);
+	hi("isjfsdofj");
 
 //	std::cout << api.get_media_likers("2200835617808968768_1992029340") << std::endl;
 //	std::cout << api.get_media_comments("2200835617808968768_1992029340") << std::endl;
