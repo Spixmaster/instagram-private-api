8f716bae1c9d9576b166f79a49cdc91e50b202ab
Spixmaster <spixmaster528@gmail.com>
2019-12-21 09:11:36 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,14 @@
 #include <iostream>
 #include "ig/Api.h"
 #include "tools/Tools.h"
-#include <variant>
-
-void hi(std::variant<std::string, int> hi)
-{
-	std::cout << "juten tag" << std::endl;
-}
 
 int main()
 {
 	ig::Api api("hija_look", "generic_password");
-
-	hi(5);
-	hi("isjfsdofj");
+	api.login();
 
 //	std::cout << api.get_media_likers("2200835617808968768_1992029340") << std::endl;
-//	std::cout << api.get_media_comments("2200835617808968768_1992029340") << std::endl;
+	std::cout << api.get_media_comments("2200835617808968768_1992029340") << std::endl;
 
 	return 0;
 }
