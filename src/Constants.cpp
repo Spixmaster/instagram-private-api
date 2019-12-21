4b6991d75edc3376229ddac8ef15b86d72d4963c
Matheus Gabriel Werny de Lima <matheusgwdl@protonmail.com>
2019-12-21 10:42:46 +0100

    submodules updated

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -4,8 +4,8 @@
 
 namespace ig
 {
-	const std::string Constants::version = "0.0.16";
-	const std::string Constants::release = "20.Dec.2019";
+	const std::string Constants::version = "0.0.17";
+	const std::string Constants::release = "21.Dec.2019";
 
 	const std::string Constants::ig_url = "https://i.instagram.com/api/v1/";
 	const std::string Constants::ig_sig_key_version = "4";
