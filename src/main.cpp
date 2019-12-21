ed9dae2c493bd4ac9ca654e0b3d919afdea869a0
Spixmaster <spixmaster528@gmail.com>
2019-12-21 22:36:37 +0100

    it works! finally

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@
 int main()
 {
 	ig::Api api("hija_look", "generic_password");
-//	api.login();
+	api.login();
 
-//	std::cout << api.get_media_likers("2200835617808968768_1992029340") << std::endl;
+	api.get_media_likers("2200835617808968768_1992029340");
 //	std::cout << api.get_media_comments("2200835617808968768_1992029340") << std::endl;
 
 	return 0;
