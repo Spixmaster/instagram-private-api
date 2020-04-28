2f1365b91e222ba7489f539ea6bb61146cf441fc
Matheus Gabriel Werny de Lima <matheusgwdl@protonmail.com>
2020-04-28 08:36:34 +0200

    Updated the readme.

--- src/constants/Constants.cpp
+++ src/constants/Constants.cpp
@@ -4,8 +4,8 @@
 
 namespace ig
 {
-	const std::string Constants::version = "0.5.0";
-	const std::string Constants::release = "27.Apr.2020";
+	const std::string Constants::version = "0.5.1";
+	const std::string Constants::release = "28.Apr.2020";
 
 	const std::string Constants::ig_api_url = "https://i.instagram.com/api/v1/";
 	const std::string Constants::ig_version = "130.0.0.31.121";
