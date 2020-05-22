32c595df6c15dad10bcb2f2a1ffb0cce28410eb9
Spixmaster <spixmaster528@gmail.com>
2020-05-22 11:27:21 +0200

    Submodule updated.

--- include/ig/Api.h
+++ include/ig/Api.h
@@ -16,6 +16,9 @@
  * @author Matheus Gabriel Werny de Lima
  * @copyright GNU General Public License v2.0
  * @version
+ * 0.5.12 (22.05.2020)
+ * - Submodule updated.
+ * @version
  * 0.5.11 (17.05.2020)
  * - Comment updated.
  * @version
